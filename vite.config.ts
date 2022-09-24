ea9b0e6a696bd1eee2edcf27267adabe7390966f
黄冠中 <574036580@qq.com>
2022-09-25 01:35:19 +0800

    打包地址

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,27 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        additionalData: `@import "${path.resolve(__dirname, 'src/assets/styles/base.less')}";`,
+export default ({ mode }: any) =>
+  defineConfig({
+    base: mode === 'production' ? 'https://huang-guanzhong.gitee.io/' : '/',
+    plugins: [solidPlugin()],
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
       },
     },
-  },
-  server: {
-    port: 8080,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          additionalData: `@import "${path.resolve(__dirname, 'src/assets/styles/base.less')}";`,
+        },
+      },
+    },
+    server: {
+      port: 8080,
+    },
+    build: {
+      target: 'esnext',
+    },
+  });
