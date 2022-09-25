aaa71d17ba5ce542765300c1a9ae59ac45610dce
黄冠中 <574036580@qq.com>
2022-09-25 18:16:12 +0800

    移动端兼容

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -2,17 +2,21 @@ import { createSignal, createRoot } from 'solid-js';
 
 type page = 'home' | 'game';
 
+const SIDE_MIN = 3;
+const SIDE_MAX = 7;
+
 function createBus() {
   const [step, setStep] = createSignal<page>('game');
-  const [grid, setGrid] = createSignal<number>(3);
   const [difficulty, setDifficulty] = createSignal<number>(1);
+
   return {
     step,
     setStep,
     difficulty,
     setDifficulty,
-    grid,
-    setGrid,
+    side: () => Math.min(SIDE_MAX, difficulty() - 1 + SIDE_MIN),
+    level: () => difficulty() * 2,
+    grid: () => Math.ceil(Math.sqrt(difficulty() * 2)) ** 2,
   };
 }
 
--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -4,25 +4,26 @@ import { TbRefresh } from 'solid-icons/tb';
 import './Game.less';
 import useBus from '~/context';
 
-const TILE_MAP = {
+const TILE_TEXT_MAP = {
   hotFace: '🥵',
-  heart: '💖',
   lemon: '🍋',
-  pepper: '🌶️',
-  meat: '🥩',
-  sun: '☀️',
-  star: '⭐',
-  planet: '🪐',
-  unicorn: '🦄',
-  turtle: '🐢',
   dragon: '🐲',
-  whale: '🐳',
-  sakura: '🌸',
-  fourLeafClover: '🍀',
   diamond: '💎',
+  heart: '💖',
+  sun: '☀️',
+  turtle: '🐢',
+  whale: '🐳',
+  pepper: '🌶️',
+  star: '⭐',
+  fourLeafClover: '🍀',
+  unicorn: '🦄',
+  meat: '🥩',
+  planet: '🪐',
+  sakura: '🌸',
 } as const;
 
-type tileKey = keyof typeof TILE_MAP;
+type tileKey = keyof typeof TILE_TEXT_MAP;
+
 enum statusType {
   'pending',
   'collect',
@@ -41,31 +42,24 @@ interface ITile {
   status?: { type: statusType };
 }
 
-const TILE_MAX = 81;
-const SIDE_LENGTH = 9;
-const SIZE = 40;
+const SIZE = 50;
 const GAP = 2;
 const PLACE_MAX = 7;
 
-const repeat = (times: number, callback: (item: any, index: number) => void) => {
-  Array.from(Array(times)).forEach(callback);
-};
 const getIndexArray = (length: number) => Array.from(Array(length)).map((_, i) => i);
 
 const Game: Component = () => {
-  const { setStep, difficulty, grid } = useBus;
-  const keys = Object.keys(TILE_MAP) as Array<tileKey>;
+  const { setStep, side, level, grid } = useBus;
+  const keys = Object.keys(TILE_TEXT_MAP) as tileKey[];
   const getRandomItem = <T extends any>(arr: T[]) => arr[~~(arr.length * Math.random())];
-  const isTopLevel = (zIndex: number) => zIndex && zIndex % 2;
 
   const initTileList = () => {
-    const res: ITile[][] = [];
     const getCol = (pos: number, side: number) => pos % side;
     const getRow = (pos: number, side: number) => ~~(pos / side);
-    const getPosByCR = (col: number, row: number) => row * SIDE_LENGTH + col;
+    const getPosByCR = (col: number, row: number) => row * side() + col;
     const getGridIndex = (currentList: ITile[], position: number, ignore: number = -1) => {
-      const row = getRow(position, SIDE_LENGTH);
-      const col = getCol(position, SIDE_LENGTH);
+      const row = getRow(position, side());
+      const col = getCol(position, side());
       const topLeft = currentList.find(item => item.position === getPosByCR(col - 1, row - 1));
       const top = currentList.find(item => item.position === getPosByCR(col, row - 1));
       const topRight = currentList.find(item => item.position === getPosByCR(col + 1, row - 1));
@@ -97,55 +91,54 @@ const Game: Component = () => {
       return getRandomItem(gridArr);
     };
     const getTransform = (position: number, gridIndex: number) => {
-      const left = getCol(position, SIDE_LENGTH) * SIZE;
+      const left = getCol(position, side()) * SIZE;
       const gridLeft = getCol(gridIndex, grid()) * (SIZE / grid());
-      const top = getRow(position, SIDE_LENGTH) * SIZE;
+      const top = getRow(position, side()) * SIZE;
       const gridTop = getRow(gridIndex, grid()) * (SIZE / grid());
       return {
         left: left + gridLeft,
         top: top + gridTop,
       };
     };
-    const getLevel = (zIndex: number) => {
+
+    const res: ITile[][] = [];
+    const remainder: tileKey[] = [];
+    const keyTimes: Partial<Record<tileKey, number>> = {};
+    for (let zIndex = 0; zIndex < level(); zIndex++) {
+      const ketTimesList = Object.keys(keyTimes) as tileKey[];
+      ketTimesList.forEach(key => {
+        const value = keyTimes[key];
+        if (value && value % 3) {
+          for (let i = 0; i < 3 - (value % 3); i++) {
+            remainder.push(key);
+          }
+        }
+      });
+
       const preList = res[zIndex - 1];
       const list: ITile[] = [];
-      repeat(TILE_MAX, (_, index) => {
+      for (let index = 0; index < side() ** 2; index++) {
         const preGridIndex = preList?.find(it => it.position === index)?.gridIndex || -1;
-        const key = getRandomItem(keys);
+        const key = remainder.length
+          ? remainder.splice(~~(remainder.length * Math.random()), 1)[0]
+          : getRandomItem(keys);
+        keyTimes[key] = keyTimes[key] || 0 + 1;
         const gridIndex = getGridIndex(list, index, preGridIndex);
-        if (gridIndex === undefined || Math.random() < 0.4) return;
-        list.push({
-          id: `${zIndex}-${index}`,
-          text: TILE_MAP[key],
-          position: index,
-          gridIndex,
-          zIndex,
-          key,
-          ...getTransform(index, gridIndex),
-        });
-      });
-      if (isTopLevel(zIndex)) {
-        const keyTimes: Partial<Record<tileKey, number>> = {};
-        preList.forEach(({ key }) => {
-          keyTimes[key] = keyTimes[key] || 0 + 1;
-        });
-        const ketTimesList = Object.keys(keyTimes) as Array<tileKey>;
-        ketTimesList.forEach(key => {
-          const value = keyTimes[key];
-          if (value && value % 3) {
-            repeat(value % 3, () => {
-              const deleteIndex = list.findIndex(it => it.key === key);
-              list.splice(deleteIndex, 1);
-            });
-          }
-        });
+        if (gridIndex !== undefined || Math.random() > 0.4) {
+          list.push({
+            id: `${zIndex}-${index}`,
+            text: TILE_TEXT_MAP[key],
+            position: index,
+            gridIndex,
+            zIndex,
+            key,
+            ...getTransform(index, gridIndex),
+          });
+        }
       }
-      return list;
-    };
+      res.push(list);
+    }
 
-    repeat(difficulty(), (_, zIndex) => {
-      res.push(getLevel(zIndex));
-    });
     return res;
   };
 
@@ -175,7 +168,10 @@ const Game: Component = () => {
 
   const renderTileGroup = () => (
     <div class="tile-wrapper">
-      <div class="tile-group">
+      <div
+        class="tile-group"
+        style={{ width: `${(side() + 1) * 50}px`, height: `${(side() + 1) * 50}px` }}
+      >
         <For each={tileList()}>
           {(levelItem, zIndex) => (
             <div class="level" style={{ 'z-index': zIndex() }}>
