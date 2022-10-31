3b808ce819cd24900b8d456f2700814ae4acc346
黄冠中 <574036580@qq.com>
2022-10-31 14:45:29 +0800

    vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default ({ mode }: any) =>
   defineConfig({
-    base: '/',
+    base: mode === 'production' ? 'https://huangguanzhong.xyz/' : '/',
     plugins: [solidPlugin()],
     resolve: {
       alias: {
