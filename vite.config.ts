15a197809d77f0ef489e9369af954393f58a250b
黄冠中 <574036580@qq.com>
2022-11-02 19:07:53 +0800

    vite /

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,27 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import path from 'path';
 
-export default ({ mode }: any) =>
-  defineConfig({
-    base: mode === 'production' ? 'https://huangguanzhong.xyz/' : '/',
-    plugins: [solidPlugin()],
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, 'src'),
-        root: path.resolve(__dirname, './'),
+export default defineConfig({
+  base: '/',
+  plugins: [solidPlugin()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      root: path.resolve(__dirname, './'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: `@import "${path.resolve(__dirname, 'src/assets/styles/base.less')}";`,
       },
     },
-    css: {
-      preprocessorOptions: {
-        less: {
-          javascriptEnabled: true,
-          additionalData: `@import "${path.resolve(__dirname, 'src/assets/styles/base.less')}";`,
-        },
-      },
-    },
-    server: {
-      port: 8080,
-    },
-    build: {
-      target: 'esnext',
-    },
-  });
+  },
+  server: {
+    port: 8080,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
